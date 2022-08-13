2ececa92ca56980b76305294d6f7660cbeb53e26
Sergey Pyatnitsky <seruyfs@mail.ru>
2022-08-13 15:42:38 +0300

    Initial commit

---
+++ src/main/java/com/personnel_accounting/PersonnelAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.personnel_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personnel_accounting/PersonnelAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personnel_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
