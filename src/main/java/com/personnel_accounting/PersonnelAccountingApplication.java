008773acc4ece4b4a9361d1b209234e8f33c25da
Сергей <63928767+sergey-pyatnitsky@users.noreply.github.com>
2022-10-06 01:44:09 +0300

    Edited dependencies and properties for future logic

--- src/main/java/com/personnel_accounting/PersonnelAccountingApplication.java
+++ src/main/java/com/personnel_accounting/PersonnelAccountingApplication.java
@@ -2,8 +2,10 @@ package com.personnel_accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.personnel_accounting.repository")
 public class PersonnelAccountingApplication {
 
     public static void main(String[] args) {
